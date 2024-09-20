c458368410f4316241044802eb17cca2af65d282
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-09-20 19:06:49 +0100

    feat: Add utils submodule

---
+++ utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+import "fmt"
+
+func TODO(msg string) {
+	fmt.Printf("<<- --- --- --- --- --- --- --- ->>\n")
+	fmt.Printf("Remember to fix this!!\n%v\n", msg)
+	fmt.Printf("<<- --- --- --- --- --- --- --- ->>\n")
+}
